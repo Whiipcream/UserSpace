df061f47010130bf6f4a76edff2ee93f15c7685c
Whiipcream <keith.champagne93@gmail.com>
2025-06-09 17:06:44 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,37 +1,64 @@
-// Firebase config + initialization
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  onSnapshot,
-  addDoc,
-  deleteDoc,
-  doc,
-  updateDoc,
-  getDocs,
-  getDoc,
-} from "https://www.gstatic.com/firebasejs/11.9.0/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
   getAuth,
   onAuthStateChanged,
-  signOut,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  getStorage,
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIZKKu6y00Q5QpaEzaIddoPD844QnUPBk",
-  authDomain: "userspace-c42b3.firebaseapp.com",
-  projectId: "userspace-c42b3",
-  storageBucket: "userspace-c42b3.firebasestorage.app",
-  messagingSenderId: "201689446744",
-  appId: "1:201689446744:web:13e2062781c517189f4d2d"
+  apiKey: "AIzaSyD-Uo84sAcOeKyXDilFZxW4W-AhvZ6JWus",
+  authDomain: "socialwave-app.firebaseapp.com",
+  projectId: "socialwave-app",
+  storageBucket: "socialwave-app.appspot.com",
+  messagingSenderId: "384197971951",
+  appId: "1:384197971951:web:fe663c8eb4d62e60b8cb92",
+  measurementId: "G-8V2PBZJGSP"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const auth = getAuth();
+const db = getFirestore();
+const storage = getStorage();
 
-export { db, auth, collection, query, where, onSnapshot, addDoc, deleteDoc, doc, updateDoc, getDocs, getDoc, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword };
+export {
+  auth,
+  db,
+  storage,
+  storageRef,
+  uploadBytes,
+  getDownloadURL,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  orderBy,
+  onAuthStateChanged,
+  signOut
+};
